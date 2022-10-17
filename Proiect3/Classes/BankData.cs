3e0240ed26b61f83ef7c5a6125d2178b6524e749
CatalinIagar <cataliniagar2000@gmail.com>
2022-10-17 10:24:28 +0300

    Initial commit

---
+++ Proiect3/Classes/BankData.cs
@@ -0,0 +1,23 @@
+﻿namespace Proiect3
+{
+    internal class BankData
+    {
+        public double age;
+        public double job;
+        public double relation;
+        public double education;
+        public double isDefault;
+        public double balance;
+        public double housing;
+        public double loan;
+        public double contact;
+        public double day;
+        public double month;
+        public double duration;
+        public double campaign;
+        public double pdays;
+        public double previus;
+        public double poutcome;
+        public double outcome;
+    }
+}
---
+++ Proiect3/Classes/DataEnums.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect3.Classes
+{
+    internal class DataEnums
+    {
+        public enum Job{
+            unemployed,
+            services,
+            management,
+            bluecollar,
+            selfemployed,
+            technician,
+            entrepreneur,
+            admin,
+            student,
+            housemaid,
+            retired,
+            unknown
+        }
+
+        public enum MaritalStatus
+        {
+            married,
+            single,
+            divorced
+        }
+
+        public enum Education
+        {
+            primary,
+            secondary,
+            tertiary,
+            unknown
+        }
+
+        public enum Default
+        {
+            no, 
+            yes
+        }
+
+        public enum Housing
+        {
+            no,
+            yes
+        }
+
+        public enum Loan
+        {
+            no,
+            yes
+        }
+
+        public enum Contact
+        {
+            cellular,
+            unknown,
+            telephone
+        }
+
+        public enum Month
+        {
+            jan, 
+            feb,
+            mar,
+            apr,
+            may,
+            jun,
+            jul,
+            aug,
+            sep,
+            oct,
+            nov,
+            dec
+        }
+
+        public enum PrevoisOutcome
+        {
+            unknown,
+            failure, 
+            success,
+            other
+        }
+
+        public enum Outcome
+        {
+            no,
+            yes
+        }
+    }
+}
